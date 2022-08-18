2f816699f2096ca8fb81c18db51621868ee689bf
Antonyefron <antonyefron007@gmail.com>
2022-08-18 18:55:13 +0530

    card section complected with overflow correction and input correct detials

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 function homiename()
    {
     var homie = document.getElementById('homiename').value;
-    document.getElementById('name').innerHTML ="Hello " + homie;
-    
+    document.getElementById('name').innerHTML ="Hello "+homie+" nice to meet YOU..";
    }
   
 
+   const loadText = document.querySelector('#selfquote')
+
+   let load = 0
+   
+   let int = setInterval(blurring, 30)
+   function blurring() {
+      load++
+      if(load > 99){
+        clearInterval (int)
+     }
+      
+      loadText.style.opacity = scale(load, 0, 100, 0, 1)
+     
+      //bg.style.filter = blur(${scale(load,0 ,100, 30, 0)}px)
+      //collegeName.style.opacity = scale(load, 0, 100, 0, 1)
+   }
+   //https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers -->
+   function scale (number, inMin, inMax, outMin, outMax) {
+       return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+   }
